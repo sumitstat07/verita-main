d2d38b57cfb4451b77b1e8ae47292a64e74fc183
Sumit Sana <sumitsana2002@gmail.com>
2026-07-16 14:24:50 +0530

    voicepipeline

--- src/screens/student_screen.py
+++ src/screens/student_screen.py
@@ -4,13 +4,23 @@ from src.components.header import header_dashboard
 from src.components.footer import footer_dashboard
 from PIL import Image
 import numpy as np
+from src.pipelines.face_pipeline import predict_attendence,get_face_embeddings,train_classifier
+from src.pipelines.voice_pipeline import get_voice_embedding
+from src.database.db import get_all_students,create_student
+import time
 
-
+def student_dashboard():
+      st.header("DASHBOARD HERE")
 
 def student_screen():
 
     style_background_dashboard()
     style_base_layout()
+
+
+    if "student_data" in st.session_state:
+          student_dashboard()
+          return
 
     c1,c2=st.columns(2,vertical_alignment="center",gap="xxlarge")
 
@@ -26,12 +36,93 @@ def student_screen():
     st.header("Login using FaceID",text_alignment="center")
     st.space()
     st.space()
-    
+
+
+    show_registration=False 
 
     photo_source= st.camera_input("Position Your face in the center")
 
     if photo_source:
-          np.array(Image.open(photo_source))
+          img=np.array(Image.open(photo_source))
+
+          with st.spinner("AI is scanning...."):
+                detected,all_ids,num_faces=predict_attendence(img)
+
+                if num_faces==0:
+                      st.warning("Face not found !")
+                elif num_faces>1:
+                      st.warning("Multiple faces found")
+
+
+                else:
+                      if detected:
+                            student_id=list(detected.keys())[0]
+                            all_students=get_all_students()
+                            student=next((s for s in all_students if s["student_id"]==student_id),None)
+
+                            if student:
+                                  st.session_state.is_logged_in=True
+                                  st.session_state.user_role="student"
+                                  st.session_state.student_data=student
+                                  st.toast(f"Welcome Back {student["name"]} !")
+                                  time.sleep(1)
+                                  st.rerun()
+
+                      else:
+                            st.info("Face not recognized ! You might be a new student !")
+                            show_registration=True
+    
+                
+
+    if show_registration:
+          with st.container(border=True):
+           st.header("Register new Profile")
+          new_name=st.text_input("Enter your name",placeholder="e.g Sumit Sarkar")
+
+          st.subheader("Optional: Voice Enrollment")
+          st.info("Enroll you for voice only attendence")
+
+
+          audio_data=None
+
+          try:
+                audio_data=st.audio_input("Record a short phrase like I am Present, My name is Akash")
+
+          except Exception:
+                st.error("Audio Data Failed !")
+
+          if st.button("Create Account",type="primary"):
+                if new_name:
+                      with st.spinner("Creating profile...."):
+                            img=np.array(Image.open(photo_source))
+                            encodings=get_face_embedding(img)
+
+                            if encodings:
+                                  face_emb=encodings[0].tolist()
+
+                                  voice_emb=None
+                                  if audio_data:
+                                        voice_emb=get_voice_embedding(audio_data.read())
+                                  response_data=create_student(new_name,face_embeddig=face_emb,voice_embedding=voice_emb)
+                                  if response_data:
+                                        train_classifier()
+                                        st.session_state.is_logged_in=True
+                                        st.session_state.user_role="student"
+                                        st.session_state.student_data=response_data[0]
+                                        st.toast(f"Profile Created! Hi {new_name} !")
+                                        time.sleep(1)
+                                        st.rerun()
+
+                            else:
+                                  st.warning("Please enter your name!")
+                                              
+
+
+
+                else:
+                      st.warning("Please enter your name!")               
+
+
 
 
     footer_dashboard()
