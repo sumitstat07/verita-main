225ae4bbd803141a40ead4cf2964e2477a562e7f
Sumit Sana <sumitsana2002@gmail.com>
2026-07-17 09:10:05 +0530

    share subject

--- src/screens/teacher_screen.py
+++ src/screens/teacher_screen.py
@@ -5,6 +5,8 @@ from src.components.footer import footer_dashboard
 from src.components.subject_card import subject_card
 from src.database.db import check_teacher_exists,create_teacher,teacher_login,get_teacher_subjects
 from src.components.dialog_create_subject import create_subject_dialog
+from src.components.dialog_share_subject import share_subject_dialog
+
 
 
 def teacher_screen():
